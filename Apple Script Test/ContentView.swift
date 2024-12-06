2714574eeb0f8a6f80b0b181f3d62b8ed1f094ee
Iomegan <dwitt.kiel@icloud.com>
2024-12-06 23:39:31 +0100

    inital commit

--- Apple Script Test/ContentView.swift
+++ Apple Script Test/ContentView.swift
@@ -5,15 +5,32 @@
 //  Created by Daniel Witt on 06.12.24.
 //
 
+import Foundation
 import SwiftUI
 
 struct ContentView: View {
+    @State private var appleScript: String = "tell application \"Finder\" to get version"
+    @State private var result: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Script", text: $appleScript)
+            Button("Run Apple Script") {
+                let appleScript = appleScript
+                var error: NSDictionary?
+
+                if let scriptObject = NSAppleScript(source: appleScript) {
+                    let result = scriptObject.executeAndReturnError(&error)
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else {
+                        print("Result: \(result.stringValue ?? "No result")")
+                        self.result = result.stringValue ?? "No result"
+                    }
+                }
+            }
+            LabeledContent("Result:") {
+                Text(result)
+            }
         }
         .padding()
     }
