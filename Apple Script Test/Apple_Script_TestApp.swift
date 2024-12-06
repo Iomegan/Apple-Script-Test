c3e46a9c0054feae49a65bb43398dc0fb05f816f
Iomegan <dwitt.kiel@icloud.com>
2024-12-06 23:30:10 +0100

    Initial Commit

---
+++ Apple Script Test/Apple_Script_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Script_TestApp.swift
+//  Apple Script Test
+//
+//  Created by Daniel Witt on 06.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Script_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
